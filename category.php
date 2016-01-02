55a21f59494b33dbdc628cabc7087e9e9a3c9122
Alpha Lu <alphaluf@outlook.com>
2016-01-02 11:08:09 +0800

    category issue

--- category.php
+++ category.php
@@ -11,36 +11,40 @@
     </div>
 
     <?php
-      // the query to set the posts per page to 3
-      $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-      $args = array('posts_per_page' => 3, 'paged' => $paged );
-      query_posts($args);
+    $paged = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1;
+    $category_name = get_query_var('category_name');
+    $args = array(
+      'posts_per_page' => 6,
+      'category_name' => $category_name,
+      'paged' => $paged,
+    );
+    $wp_query = new WP_Query( $args );
     ?>
-    <?php if (have_posts()) {?>
-
-      <?php while (have_posts()) : the_post(); ?>
+    <?php if ($wp_query->have_posts()) {?>
+      <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
       <div class="col-sm-3">
         <?php if ( has_post_thumbnail() ) {?>
           <a class="thumbnail" href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
         <?php } ?>
       </div>
       <?php endwhile;?>
-      <?php the_posts_pagination(); ?>
+
     <?php }?>
   </div>
   <div class="row">
     <p class="pull-right">
       <?php
-        // $big = 999999999; // need an unlikely integer
-        //
-        // echo paginate_links( array(
-        // 	'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-        // 	'format' => '?paged=%#%',
-        // 	'current' => max( 1, get_query_var('paged') ),
-        // 	'total' => $wp_query->max_num_pages
-        // ) );
-      ?>
+        global $wp_query;
+        $big = 999999999; // need an unlikely integer
 
+        echo paginate_links( array(
+        	'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+        	'format' => '?paged=%#%',
+        	'current' => max( 1, get_query_var('paged') ),
+        	'total' => $wp_query->max_num_pages
+        ) );
+      ?>
+      <?php wp_reset_query();?>
     </p>
   </div>
 
